5f7957315155a1cf53ae2f78b8bcd1f9aaed3ed7
Jiapeng Li <jli@hytrust.com>
2018-02-09 18:10:13 -0800

    Url config and View

--- tutorial/view.py
+++ tutorial/view.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
+
 
 def hello(request):
     context = {}
     context['hello'] = 'Hello World!'
     return render(request, 'hello.html', context)
+
+
+def currentTime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
